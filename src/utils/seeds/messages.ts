f0d0c8ceb5a799d9b7e0ab4272ddb113d01af536
Nicolas Vergara <nvergara1628@gmail.com>
2024-08-06 20:52:14 -0500

    Improving message system
    - Adding error message to unexpected commands
    - Moving message text to output array with new format to trim
    
     On branch database
    
     Changes to be committed:
    	modified:   src/db/models/message.model.ts
    	modified:   src/services/message.service.ts
    	modified:   src/utils/seeds/messages.ts

--- src/utils/seeds/messages.ts
+++ src/utils/seeds/messages.ts
@@ -1,21 +1,38 @@
 import { v4 as uuidv4 } from 'uuid';
-import { MessageAttributes } from "../../db/models/message.model";
 
 export const messagesValRoom = [
     { 
         id: uuidv4(),
         location_id: 1, 
         message_index: 0, 
-        message_text: 'What do you want to do?',
         next_level: false,
-        output: ['/sendAudio1', '/sendAudio2', '/insertImageVal1', '/sendAudio3', '/sendMapValRoom'],
+        output: [
+            '/send*No has seleccionado la opcion correcta. Por favor, intenta de nuevo.'
+        ],
+        command: '/error',
+    },
+    { 
+        id: uuidv4(),
+        location_id: 1, 
+        message_index: 0, 
+        next_level: false,
+        output: [
+            '/sendAudio1',
+            '/sendAudio2',
+            '/insertImageVal1',
+            '/sendAudio3',
+            '/sendMapValRoom',
+            '/send*What do you want to do?'
+        ],
         command: '/start',
     },
     {
         id: uuidv4(),
         location_id: 1, 
         message_index: 0, 
-        message_text: "No me puedo quedar en cama. Debería ir a ver qué necesita papá.",
+        output: [
+            '/send*No me puedo quedar en cama. Debería ir a ver qué necesita papá.'
+        ],
         next_level: false,
         command: '/a1',
     },
@@ -23,98 +40,115 @@ export const messagesValRoom = [
         id: uuidv4(),
         location_id: 1, 
         message_index: 0, 
-        message_text: "¡AUCH!\nAntes de poder pensar qué querrá papá, te tropiezas con la alfombra y peleas contra la gravedad hasta terminar en la puerta y recuperar tu balance.",
         next_level: false,
+        output: [
+            '/send*¡AUCH!\nAntes de poder pensar qué querrá papá, te tropiezas con la alfombra y peleas contra la gravedad hasta terminar en la puerta y recuperar tu balance.'
+        ],
         command: '/a2',
     },
     {
         id: uuidv4(),
         location_id: 1, 
         message_index: 0, 
-        message_text: "Voy a buscar a papá. Debe estar en la sala.",
         next_level: true,
         next_locations: 'Inside the house',
-        output: ['/sendMapDownstairs'],
+        output: [
+            '/sendMapDownstairs',
+            '/send*Voy a buscar a papá. Debe estar en la sala.'
+        ],
         command: '/a3',
     },
     {
         id: uuidv4(),
         location_id: 1, 
         message_index: 0, 
-        message_text: "No debería salir de casa todavía. ¿Dónde está papá?",
         next_level: false,
+        output: [
+            '/send*No debería salir de casa todavía. ¿Dónde está papá?'
+        ],
         command: '/a4',
     },
     {
         id: uuidv4(),
         location_id: 1, 
         message_index: 0, 
-        message_text: "",
         next_level: false,
-        output: ['/sendImageWorkshopEmpty'],
+        output: [
+            '/sendImageWorkshopEmpty'
+        ],
         command: '/b1',
     },
     {
         id: uuidv4(),
         location_id: 1, 
         message_index: 0, 
-        message_text: "",
         next_level: false,
-        output: ['/sendImageKitchen'],
+        output: [
+            '/sendImageKitchen'
+        ],
         command: '/b2',
     },
     {
         id: uuidv4(),
         location_id: 2, 
         message_index: 1, 
-        message_text: "",
         next_level: false,
-        output: ['/sendImageWorkshopEmpty'],
+        output: [
+            '/sendImageWorkshopEmpty'
+        ],
         command: '/b1',
     },
     {
         id: uuidv4(),
         location_id: 2, 
         message_index: 1, 
-        message_text: "",
         next_level: false,
-        output: ['/sendImageDadRoom'],
+        output: [
+            '/sendImageDadRoom'
+        ],
         command: '/a1',
     },
     {
         id: uuidv4(),
         location_id: 2, 
         message_index: 1, 
-        message_text: "No puedo regresar a mi cuarto todavia. Debo encontrar a papá.",
         next_level: false,
+        output: [
+            '/send*No puedo regresar a mi cuarto todavia. Debo encontrar a papá.'
+        ],
         command: '/a2',
     },
     {
         id: uuidv4(),
         location_id: 2, 
         message_index: 1, 
-        message_text: "No debería salir de casa todavía. ¿Dónde está papá?",
         next_level: false,
+        output: [
+            '/send*No debería salir de casa todavía. ¿Dónde está papá?'
+        ],
         command: '/a3',
     },
     {
         id: uuidv4(),
         location_id: 2, 
         message_index: 1, 
-        message_text: "",
         next_level: false,
-        output: ['/sendImageKitchen'],
+        output: [
+            '/sendImageKitchen'
+        ],
         command: '/b2',
     },
     {
         id: uuidv4(),
         location_id: 2, 
         message_index: 1, 
-        message_text: `Gracias por llegar tan rápido, Val.Tengo algo importante que encargarte.
-                    \nHoy es la coronación de la nueva reina, Mipha, y necesitamos llevarle un regalo de parte nuestra.
-                    \nEntra al taller, escoge la escultura que más te guste y llévala de inmediato a la ceremonia.`,
-        next_level: true,
-        output: ['/sendMapDownstairs'],
+        next_level: false,
+        output: [
+            '/sendMapDownstairs',
+            `/send*Gracias por llegar tan rápido, Val.Tengo algo importante que encargarte.
+            \nHoy es la coronación de la nueva reina, Mipha, y necesitamos llevarle un regalo de parte nuestra.
+            \nEntra al taller, escoge la escultura que más te guste y llévala de inmediato a la ceremonia.`
+        ],
         next_locations: 'Living Room',
         command: '/b2',
     },
@@ -122,9 +156,10 @@ export const messagesValRoom = [
         id: uuidv4(),
         location_id: 5, 
         message_index: 2, 
-        message_text: "",
         next_level: true,
-        output: ['/sendMapWorkshop'],
+        output: [
+            '/sendMapWorkshop'
+        ],
         next_locations: 'Carpentry Workshop',
         command: '/b1',
     },
@@ -132,43 +167,51 @@ export const messagesValRoom = [
         id: uuidv4(),
         location_id: 5, 
         message_index: 2, 
-        message_text: "",
         next_level: false,
-        output: ['/sendImageDadRoom'],
+        output: [
+            '/sendImageDadRoom'
+        ],
         command: '/a1',
     },
     {
         id: uuidv4(),
         location_id: 5, 
         message_index: 2, 
-        message_text: "Mmmm, no necesito nada del cuarto por el momento. Mejor voy al taller por una escultura.",
         next_level: false,
+        output: [
+            '/send*Mmmm, no necesito nada del cuarto por el momento. Mejor voy al taller por una escultura.'
+        ],
         command: '/a2',
     },
     {
         id: uuidv4(),
         location_id: 5, 
         message_index: 2, 
-        message_text: "No puedo salir de casa sin la escultura. Val... ¡enfócate!",
         next_level: false,
+        output: [
+            '/send*No puedo salir de casa sin la escultura. Val... ¡enfócate!'
+        ],
         command: '/a3',
     },
     {
         id: uuidv4(),
         location_id: 5, 
         message_index: 2, 
-        message_text: "",
         next_level: false,
-        output: ['/sendImageKitchen'],
+        output: [
+            '/sendImageKitchen'
+        ],
         command: '/b2',
     },
     {
         id: uuidv4(),
         location_id: 5, 
         message_index: 2, 
-        message_text: "Pero... ¿cómo me voy a quedar en la sala? ¡Voy por la escultura!",
         next_level: false,
-        output: ['/sendImageKitchen'],
+        output: [
+            '/sendImageKitchen',
+            '/send*Pero... ¿cómo me voy a quedar en la sala? ¡Voy por la escultura!'
+        ],
         command: '/b3',
     }
 ];
