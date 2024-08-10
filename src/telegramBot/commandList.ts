52e87a1f60e2df1a782e2cb65d6e1818e693cad4
Sebastian Guaqueta <scguaquetam@gmail.com>
2024-08-10 12:08:28 -0500

    feat: returning nft id on the mint response

--- src/telegramBot/commandList.ts
+++ src/telegramBot/commandList.ts
@@ -6,12 +6,13 @@ import Web3Service from "../services/web3.service"
 import AttestationService, { AttestationData } from "../services/attestation.service"
 import { AttestationAttributes } from "../db/models/attestation.model"
 import { stickers } from "../utils/sticker"
+import NFTService, { NftMintResponse } from "../services/nft.service"
 
 const userService = new UserService()
 const web3Service = new Web3Service()
 const messageService = new MessageService()
 const attestationService = new AttestationService()
-
+const nftService = new NFTService()
 
 exports.newAddress = async function (chatId: string, bot: TelegramBot, msg: TelegramBot.Message) {
     try {
@@ -106,12 +107,30 @@ exports.deleteAccount = async function (chatId: string, bot: TelegramBot, msg: T
     bot.sendMessage(msg.chat.id, generalMessages['error']['errorGeneric'])
     }
 }
+//TEST ONLY FUNCTION
+// exports.mintNft = async function (chatId: string, bot: TelegramBot, msg: TelegramBot.Message) {
+//   try {
+//     console.log('Minting NFT');
+//     const mintInfo : NftMintResponse | null = await nftService.mintNFT({
+//       to: "0x9DA1cC61b11FD1AB42F6bB26ac97C13187312A40",
+//       id: chatId,
+//       index: 0,
+//       location: "Test location",
+//       data: "0x"
+//     })
+//     console.log('hash:', mintInfo);
+//   } catch (error) {
+//     console.log('Error on mintnft:', error)
+//     bot.sendMessage(msg.chat.id, generalMessages['error']['errorGeneric'])
+//   }
+// }
 
 const commandList : { [key: string]: any } = {
     newAddress: exports.newAddress,
     help: exports.help,
     submitAttestation: exports.submitAttestation,
     delete: exports.delete,
+    // mintNft: exports.mintNft,
 } 
 
 export default commandList;
